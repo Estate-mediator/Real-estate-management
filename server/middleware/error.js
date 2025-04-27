09194b0be7045925c440260e252793fa080162fc
unknown <dasabhirup153@gmail.com>
2025-04-27 15:35:29 +0530

    created a frontend folder

--- server/middleware/error.js
+++
@@ -1,9 +0,0 @@
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    
-    res.status(500).json({
-      success: false,
-      message: err.message || 'Server Error',
-      error: process.env.NODE_ENV === 'development' ? err : {}
-    });
-  };
